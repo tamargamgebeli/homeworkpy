61b8ebf6b0cab54bc81815cd229e938e6eaded25
tamargamgebeli <tamar.gamgebeli.1@iliauni.edu.ge>
2024-11-25 23:56:24 +0400

    Add files via upload

---
+++ homework.2.py
@@ -0,0 +1,23 @@
+
+def kilometers_to_meters(kilometers):
+    meters=0.62*kilometers
+    return meters
+result=kilometers_to_meters(5)
+print(result)
+
+def calculate_volume(heigh=1,width=1,length=1):
+    volume=heigh*width*length
+    return volume
+result=calculate_volume(5,2)
+print(result)
+
+def get_vowels(*args):
+    vowels = 'aeiou'  
+    result = []
+    for string in args:
+        if string[0] in vowels:
+            result.append(string)
+    return result  
+
+
+print(get_vowels("iaragi", "asanti"))  #
